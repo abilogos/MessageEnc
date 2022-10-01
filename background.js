7352f8e30b464e59ff69c3a54a770e5f61a13103
Abilogos <dev.abi.log@gmail.com>
2022-10-01 17:52:46 +0300

    added context menu successfully

--- background.js
+++ background.js
@@ -1,66 +1,36 @@
-browser.contextMenus.create({
+chrome.contextMenus.create({
     id: "encrypt-message",
     title: "Encrypt Message",
-    contexts: ["selection"],
-    // onClick: (info, tab) => {
-    //     let message = [text, info.frameId > 0, info.editable];
-    //     browser.tabs.sendMessage(tab.id, message)
-    //         .then(() => true, (e) => {
-    //             //if script is not loaded
-    //             return browser.tabs.executeScript(tab.id, { file: "content-script.js" })
-    //                 .then( () => browser.tabs.sendMessage(tab.id, message) );
-    //         } )
-    //         .then( () => { console.log('2r'); })
-    //         .catch(e => console.error('error message-enc : '+ e));
-    // }
+    contexts: ["selection"]
 });
-browser.contextMenus.create({
+chrome.contextMenus.create({
     id: "decrypt-message",
     title: "Decrypt Message",
     contexts: ["selection"]
 });
 
-browser.contextMenus.onClicked.addListener((info, tab) => {
-    let text;
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    let altered;
     switch (info.menuItemId) {
         case "encrypt-message":
-            // console.log(info.selectionText);
-            text = 'ff';
+            console.log(info.selectionText);
+            altered = 'e' + info.selectionText;
             break;
         case "decrypt-message":
-            // console.log(info.selectionText);
+            altered = 'd' + info.selectionText;
             break;
         default:
             return;
   }
-  
-/*    let message = [text, info.frameId > 0, info.editable];
-    browser.tabs.sendMessage(tab.id, text)
+
+    chrome.tabs.sendMessage(tab.id, altered)
         .then(() => true, (e) => {
             //if script is not loaded
-            return browser.tabs.executeScript(tab.id, { file: "content-script.js" })
-                .then( () => browser.tabs.sendMessage(tab.id, message) );
+            return chrome.scripting.executeScript({target: { tabId: tab.id },  files: ["content-script.js"] })
+                .then( () => chrome.tabs.sendMessage(tab.id, altered) );
         } )
-        .then( () => { console.log('2r'); })
-        .catch(e => console.error('error message-enc : '+ e));*/
-  
-  const code = "replaceSelection(" + "'test"+info.selectionText + "');";
-  browser.tabs.executeScript({
-            //check function has been defined
-            code: "typeof replaceSelection === 'function';",
-          }).then((results) => {
-            //if function has not been defined, load the content script
-            if (!results || results[0] !== true) {
-                return browser.tabs.executeScript(tab.id, {
-                    file: "content-script.js",
-                });
-            }
-        }).then(() => {
-            //exec the code in the content-script
-            return browser.tabs.executeScript(tab.id, {
-                code,
-            });
-        }).catch((error) => {
-            console.error("Failed to modify text: " + error);
-        });
+        .then( () => { 
+            //in case of any further operation. if not needed can be removed in the next refactor
+         })
+        .catch(e => console.error('error message-enc : '+ e));
 });
--- content-script.js
+++ content-script.js
@@ -1,36 +1,26 @@
-function replaceSelection(text){
-console.log(window);
-console.log(text);
-    // Hide the event from the page to prevent tampering.
-    // event.stopImmediatePropagation();
-
+/**
+ * replace Selected Text in the browser
+ *
+ * @author https://stackoverflow.com/a/3997896/9287628
+ * @param  string replacementText
+ * @return void
+ */
+function replaceSelectedText(replacementText) {
+    var sel, range;
+    if (window.getSelection) {
+        sel = window.getSelection();
+        if (sel.rangeCount) {
+            range = sel.getRangeAt(0);
+            range.deleteContents();
+            range.insertNode(document.createTextNode(replacementText));
+        }
+    } else if (document.selection && document.selection.createRange) {
+        range = document.selection.createRange();
+        range.text = replacementText;
+    }
 }
 
-// [a,b,c]
-//browser.runtime.onMessage.addListener((text) => {
-    // let selected = editable ? document.activeElement : window.getSelection().getRangeAt(0);
-//    console.log(window.getSelection());
-//    console.log(text);
-	// return Promise.resolve(true);
-
-	//div.style.top = div.style.left = "0px";
-	//div.children[1].removeAttribute("style");
-	//div.children[1].scrollTop = 0;
-	
-	/*div.children[1].firstChild.textContent = text;*/
-	// div.children[1].style.width = div.children[1].offsetWidth + "px";
-    // div.children[1].style.height = Math.min(root.clientHeight - 25, div.children[1].offsetHeight) + "px";
-	
-	/*let max_x = root.clientWidth - div.offsetWidth;
-	let max_y = root.clientHeight - div.offsetHeight;
-	let position = frame ? { left: max_x / 2, bottom: max_y / 2 - 10 } :
-		editable ? document.activeElement.getBoundingClientRect() :
-		window.getSelection().getRangeAt(0).getBoundingClientRect();
-	div.style.top = Math.max(0, Math.min(position.bottom + 10, max_y)) + "px";
-	div.style.left = Math.max(0, Math.min(position.left, max_x)) + "px";
-	
-	div.style.visibility = "visible";
-	document.addEventListener("click", div_hide, { once: true });
-	document.activeElement.blur();
-	return Promise.resolve(true);*/
-// });
+chrome.runtime.onMessage.addListener((text, sender, sendResponse) => {
+    replaceSelectedText(text);
+	sendResponse({status: 'ok'});
+ });
